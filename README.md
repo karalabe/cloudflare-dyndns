95c90d2c0e6bc3bac39bea87e57e2354f2423ae3
Péter Szilágyi <peterke@gmail.com>
2016-01-10 18:11:38 +0200

    Initial CloudFlare DNS updater

---
+++ README.md
@@ -0,0 +1,34 @@
+# CloudFlare Dynamic DNS Updater
+
+This is an extremely simple tool to update CloudFlare dynamic DNS entries. It's
+sole purpose is to periodically query the external IP address of the machine it
+is running on, and modify the DNS entry accordingly. Opposed to a few scripts
+out there that do a one shot update requiring regular cron sheduling, this one
+is more aimed to environments where you can configure it as a service and leave
+the updater running in the background.
+
+## Installation and usage
+
+The tool in written in Go and follows the usual installation method:
+
+```
+go get github.com/karalabe/cloudflare-dyndns
+```
+
+```
+cloudflare-dyndns --help
+
+Usage of cloudflare-dyndns:
+  -domain string
+    	Parent domain of the hostname to update
+  -hosts string
+    	Comma separated subdomain list to update
+  -key string
+    	CloudFlare authorization token
+  -ttl int
+    	Domain time to live value (default 120)
+  -update duration
+    	Time interval to run the updater (default 1m0s)
+  -user string
+    	CloudFlare username to update with
+```
---
+++ main.go
@@ -0,0 +1,180 @@
+// CloudFlare Dynamic DNS Updater
+// Copyright (c) 2015 Péter Szilágyi. All rights reserved.
+//
+// Released under the MIT license.
+
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+	"time"
+)
+
+var (
+	updateFlag = flag.Duration("update", time.Minute, "Time interval to run the updater")
+	userFlag   = flag.String("user", "", "CloudFlare username to update with")
+	keyFlag    = flag.String("key", "", "CloudFlare authorization token")
+	domainFlag = flag.String("domain", "", "Parent domain of the hostname to update")
+	hostsFlag  = flag.String("hosts", "", "Comma separated subdomain list to update")
+	ttlFlag    = flag.Int("ttl", 120, "Domain time to live value")
+)
+
+func main() {
+	flag.Parse()
+
+	previous := "" // Previous address to prevent hammering CloudFlare
+	for {
+		// Resolve the external address and update if valid
+		address, err := resolveAddress()
+		if err != nil {
+			log.Printf("Failed to resolve external address: %v", err)
+		}
+		if address != "" && address != previous {
+			log.Printf("Updating IP address to %s", address)
+
+			for _, host := range strings.Split(*hostsFlag, ",") {
+				if _, err := updateDNS(address, *userFlag, *keyFlag, *domainFlag, host, *ttlFlag); err != nil {
+					log.Printf("Failed to update %s.%s: %v", host, *domainFlag, err)
+					continue
+				}
+				log.Printf("Domain updated: %s.%s", host, *domainFlag)
+				previous = address
+			}
+		}
+		// Wait for the next invocation
+		time.Sleep(*updateFlag)
+	}
+}
+
+// resolveAddress tries to resolve the external IP address of the machine via
+// third party resolution services. Currently two are queried and the DNS entry
+// only updated if they both match.
+func resolveAddress() (string, error) {
+	// Resolve the external address via whatismyipaddress.com
+	reply, err := http.Get("http://ipv4bot.whatismyipaddress.com")
+	if err != nil {
+		return "", err
+	}
+	defer reply.Body.Close()
+
+	potential, err := ioutil.ReadAll(reply.Body)
+	if err != nil {
+		return "", err
+	}
+	// Resolve the external address via ipify.org
+	reply, err = http.Get("https://api.ipify.org")
+	if err != nil {
+		return "", err
+	}
+	defer reply.Body.Close()
+
+	confirm, err := ioutil.ReadAll(reply.Body)
+	if err != nil {
+		return "", err
+	}
+	// Confirm or discard the resolution
+	if bytes.Compare(potential, confirm) != 0 {
+		return "", fmt.Errorf("resolution conflict: %s != %s", string(potential), string(confirm))
+	}
+	return string(potential), nil
+}
+
+// updateDNS updates a single CloudFlare DNS entry to the given IP address.
+func updateDNS(address string, user, key string, zone string, record string, ttl int) (string, error) {
+	// Resolve the record id for the host
+	id, err := resolveRecordId(user, key, zone, record)
+	if err != nil {
+		return "", fmt.Errorf("record id reolution failed: %v", err)
+	}
+	// Post the CloudFlare DNS update request
+	reply, err := http.PostForm("https://www.cloudflare.com/api_json.html", url.Values{
+		"a":       {"rec_edit"},
+		"email":   {user},
+		"tkn":     {key},
+		"z":       {zone},
+		"id":      {id},
+		"type":    {"A"},
+		"name":    {record},
+		"ttl":     {strconv.Itoa(ttl)},
+		"content": {address},
+	})
+	if err != nil {
+		return "", err
+	}
+	defer reply.Body.Close()
+
+	// Parse the reply and check if an error occurred
+	body, err := ioutil.ReadAll(reply.Body)
+	if err != nil {
+		return "", err
+	}
+	var failure struct {
+		Result  string `json:"result"`
+		Message string `json:"msg"`
+	}
+	if err := json.Unmarshal(body, &failure); err != nil {
+		return "", err
+	}
+	if failure.Result == "error" {
+		return "", fmt.Errorf("request denied: %s", failure.Message)
+	}
+	// Yay, no failure, flatten the reply and return
+	return string(body), err
+}
+
+// resolveRecordId resolves the id string of a single subdomain entry in a zone
+// listing.
+func resolveRecordId(user, key string, zone, record string) (string, error) {
+	// Post a CloudFlare DNS record list request
+	reply, err := http.PostForm("https://www.cloudflare.com/api_json.html", url.Values{
+		"a":     {"rec_load_all"},
+		"email": {user},
+		"tkn":   {key},
+		"z":     {zone},
+	})
+	if err != nil {
+		return "", err
+	}
+	defer reply.Body.Close()
+
+	// Parse the reply and check if an error occurred
+	body, err := ioutil.ReadAll(reply.Body)
+	if err != nil {
+		return "", err
+	}
+	var response struct {
+		Result   string `json:"result"`
+		Message  string `json:"msg"`
+		Response struct {
+			Records struct {
+				Objs []struct {
+					Id   string `json:"rec_id"`
+					Name string `json:"display_name"`
+				} `json:"objs"`
+			} `json:"recs"`
+		} `json:"response"`
+	}
+	if err := json.Unmarshal(body, &response); err != nil {
+		return "", err
+	}
+	if response.Result == "error" {
+		return "", fmt.Errorf("request denied: %s", response.Message)
+	}
+	// Find the DNS record in the response
+	for _, obj := range response.Response.Records.Objs {
+		if obj.Name == record {
+			return obj.Id, nil
+		}
+	}
+	return "", errors.New("unknown record")
+}
